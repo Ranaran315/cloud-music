fc123a71880bbb315dd863258db24695cd15f893
ranan <ranan@email.com>
2024-08-29 09:45:15 +0800

    test: eslint、lint-staged测试

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  eslintConfigPrettier,
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-explicit-any': 'off', // 允许显性的any类型
+  //   },
+  // },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      '**/*.css',
+      '**/*.jpg',
+      '**/*.jpeg',
+      '**/*.png',
+      '**/*.gif',
+      '**/*.d.ts',
+    ],
+  },
+]
