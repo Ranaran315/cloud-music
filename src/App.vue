331cd137e6a82837bb0ede2b171e5f74a93c22f4
ranan <ranan@email.com>
2024-08-29 17:05:47 +0800

    feat: 加入vuerouter

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <h1>Hello World</h1>
-  <a href="#">1111111111</a>
-  <ul>
-    <li>1</li>
-    <li>2</li>
-  </ul>
-  <button @click="handleClick">点击</button>
-  <button @click="handleClick2">检查登录状态</button>
-  <img :src="qrImg" alt="" />
+  <router-view></router-view>
+  <div class="logo"></div>
 </template>
 
-<script setup lang="ts">
-import { useLoginStore } from '@/store'
-import { computed } from 'vue'
+<script setup lang="ts"></script>
 
-const loginStore = useLoginStore()
-const qrImg = computed(() => loginStore.qrImg)
-const handleClick = async () => {
-  loginStore.getQRCode()
+<style scoped>
+.logo {
+  height: 60px;
+  background: url('topbar.png') no-repeat 0 0;
 }
-const handleClick2 = async () => {
-  loginStore.getLoginStatus()
-}
-</script>
-
-<style scoped></style>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import router from '@/route'
 
 const store = createPinia()
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
