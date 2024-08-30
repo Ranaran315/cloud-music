8e2d140dc8cf1249778dae5900ab26a03704cc61
ranan <ranan@email.com>
2024-08-30 18:27:23 +0800

    feat: 完善search组件的搜索功能

--- src/components/input/search.vue
+++ src/components/input/search.vue
@@ -6,29 +6,32 @@
       raw
       :scrollable="false"
       :show-arrow="false"
+      placement="bottom"
     >
       <template #trigger>
-        <ra-input
-          :placeholder="placeholder"
-          v-model="modelValue"
-          ref="RaInputRef"
-          clearable
-          @focus="handleFocus"
-          @blur="handleBlur"
-          @keydown.tab="handleKeydownTab"
-          @input="handleChange"
-        >
-          <template #prefix>
-            <ra-button shape="circle">
-              <ra-icon> <Search /> </ra-icon>
-            </ra-button>
-          </template>
-        </ra-input>
+        <div :class="ucn.e('trigger')">
+          <ra-input
+            :placeholder="defaultKeyword.show"
+            v-model.trim="modelValue"
+            ref="RaInputRef"
+            clearable
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @keydown.tab="handleKeydownTab"
+            @input="handleChange"
+          >
+            <template #prefix>
+              <ra-button shape="circle">
+                <ra-icon> <Search /> </ra-icon>
+              </ra-button>
+            </template>
+          </ra-input>
+        </div>
       </template>
       <div :class="ucn.e('popover')">
-        <ul :class="ucn.e('search-list')">
+        <div :class="ucn.e('search-list')">
           <template v-if="showHotlist">
-            <div class="title">推荐热搜</div>
+            <div :class="ucn.e('title')">推荐热搜</div>
             <li
               :class="ucn.e('search-item')"
               v-for="(item, index) of hotlist"
@@ -39,16 +42,24 @@
             </li>
           </template>
           <template v-else>
-            <div class="title">猜你想搜</div>
-            <li
-              :class="ucn.e('search-item')"
-              v-for="(item, index) of searchlist"
-              :key="index"
-            >
-              <span class="word">{{ item.name }}</span>
-            </li>
+            <div :class="ucn.e('title')">猜你想搜</div>
+            <div v-for="(order, index) of searchlistOrder" :key="index">
+              <div :class="ucn.e('subtitle')">
+                <ra-icon size="1.5em">
+                  <component :is="searchlistMap[order].component"></component>
+                </ra-icon>
+                {{ searchlistMap[order].name }}
+              </div>
+              <div
+                :class="ucn.e('search-item')"
+                v-for="(item, index) of searchlist[order]"
+                :key="index"
+              >
+                <div class="word">{{ item.name }}</div>
+              </div>
+            </div>
           </template>
-        </ul>
+        </div>
       </div>
     </n-popover>
   </div>
@@ -57,9 +68,9 @@
 <script setup lang="ts">
 import { RaButton, RaIcon, RaInput } from '@capybara-plus/vue'
 import { NPopover } from 'naive-ui'
-import { Search } from '@/icons'
+import { Album, Artist, Playlist, Search, Song } from '@/icons'
 import { useClassName } from '@/hooks'
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, reactive, ref, watch, nextTick } from 'vue'
 import searchApi from '@/api/search'
 
 const ucn = useClassName('search')
@@ -74,35 +85,48 @@ const searchRef = ref(null)
 const RaInputRef = ref<typeof RaInput | null>(null)
 const handleMouseover = () => {
   RaInputRef.value?.focus()
-  showPopover.value = true
+  nextTick(() => {
+    showPopover.value = true
+  })
 }
 
+// 是否展示 popover
 const showPopover = ref(false)
 
+// 失去焦点时关闭 popover
 const handleBlur = () => {
   showPopover.value = false
 }
 
 // 获取默认搜索
-const placeholder = ref('搜索歌曲、歌手、专辑')
+const defaultKeyword = reactive({
+  show: '搜索歌曲、歌手、专辑等',
+  keyword: '',
+})
 async function getDefaultSearchKeywords() {
-  const { data } = await searchApi.default()
-  placeholder.value = data.showKeyword
+  const {
+    data: { showKeyword, realkeyword },
+  } = await searchApi.default()
+  defaultKeyword.show = showKeyword
+  defaultKeyword.keyword = realkeyword
 }
 // 挂载时获取默认搜索
 onMounted(() => {
   getDefaultSearchKeywords()
 })
 
-// 在搜索框中按下 Tab 键将 placeholder 设置为当前搜索词
+// 在搜索框中按下 Tab 键将 默认搜索关键字 设置为当前搜索词
 const handleKeydownTab = (e: KeyboardEvent) => {
   e.preventDefault()
   if (modelValue.value) return
-  modelValue.value = placeholder.value
+  modelValue.value = defaultKeyword.keyword
+  nextTick(() => {
+    search()
+  })
 }
 
 // 获取热搜列表
-const hotlist = ref<any>([])
+const hotlist = ref<any[]>([])
 let throllte = 0 // 节流
 const handleFocus = async () => {
   if (!showHotlist.value) return
@@ -118,32 +142,62 @@ const handleFocus = async () => {
 
 // 计算是否展示热搜列表
 const showHotlist = computed(() => {
-  return !modelValue.value || searchlist.value.length === 0
+  return !modelValue.value || JSON.stringify(searchlist.value) == '{}'
 })
 
-// 值发生改变时获取搜索建议
-const searchlist = ref<any>([])
-let antiShake: any = null // 防抖
+// 根据关键字获取搜索建议
+const searchlist = ref<any>({})
+const searchlistOrder = ref<string[]>([])
+const searchlistMap: Record<string, any> = {
+  albums: {
+    name: '专辑',
+    component: Album,
+  },
+  artists: {
+    name: '歌手',
+    component: Artist,
+  },
+  playlists: {
+    name: '歌单',
+    component: Playlist,
+  },
+  songs: {
+    name: '歌曲',
+    component: Song,
+  },
+}
+
+// 当值发生改变时搜索
 const handleChange = async () => {
-  if (antiShake) clearTimeout(antiShake)
-  antiShake = setTimeout(async () => {
+  search()
+}
+
+// 根据关键字搜索
+let searchShake: any = null // 防抖
+const search = async () => {
+  if (searchShake) clearTimeout(searchShake)
+  searchShake = setTimeout(async () => {
     try {
       const res = (await searchApi.suggest(modelValue.value)) as any
       const { result } = res
-      result?.order?.forEach((item: string) => {
-        searchlist.value.push(...result[item])
+      searchlistOrder.value = result?.order
+      searchlistOrder.value?.forEach?.((item: string) => {
+        searchlist.value[item] = result[item]
       })
+      console.log(searchlist)
     } catch (error) {
       console.error(error)
     }
   }, 300)
 }
 
+// 当搜索框的值为空时，清空搜索列表
 watch(
   () => modelValue.value,
   (val: string) => {
     if (val.length === 0) {
-      searchlist.value = []
+      searchlist.value = {}
+      searchlistOrder.value = []
     }
   }
 )
