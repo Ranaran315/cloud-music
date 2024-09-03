a405a5e79690f251fa214466cceab3251859374e
ranan <ranan@email.com>
2024-09-03 17:04:01 +0800

    feat: 加入用户接口

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,19 +1,18 @@
 import { userApi } from '@/api'
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
-import { useLoginStore } from './login'
+import { User } from '@/utils/type'
 
 export const useUserStore = defineStore('user', () => {
-  const userInfo = ref<any>({}) // 用户信息
+  const userInfo = ref<User | any>({}) // 用户信息
   const profile = computed(() => userInfo.value?.profile)
 
   // 获取用户信息
   const getUserInfo = async () => {
     try {
-      await useLoginStore().getLoginStatus()
       const {
         account: { id },
-      } = (await userApi.getAccount()) as any
+      } = await userApi.getAccount() // 登陆后可调用该接口获取用户 id
       const res = await userApi.getUserDetail(id)
       userInfo.value = res
     } catch (error) {
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -10,6 +10,47 @@ export function isBoolean(val: any): val is boolean {
   return typeof val === 'boolean'
 }
 
+// 用户接口
+export interface Profile {
+  avatarUrl: string // 头像
+  backgroundUrl: string // 背景
+  birthday: number // 生日
+  city: number // 城市
+  province: number // 省份
+  defaultAvatar: boolean // 是否默认头像
+  followed: boolean // 是否关注
+  gender: number // 性别
+  nickname: string // 昵称
+  signature: string // 签名
+  userId: number // 用户 id
+  userType: number // 用户类型
+  vipType: number // vip 类型
+}
+// 账户信息
+export interface Account {
+  status: number
+}
+// 登录后返回的用户登录信息
+export interface LoginUser {
+  account: Account
+  profile: Profile & {
+    lastLoginIP: string // 最后登录 IP
+    lastLoginTime: number // 最后登录时间
+  }
+}
+// 用户详情
+export interface User {
+  level: number // 等级
+  listenSongs: number // 听歌数量
+  createTime: number // 创建时间
+  profile: Profile & {
+    followeds: number // 粉丝数
+    follows: number // 关注数
+    playlistCount: number // 歌单数量
+  } // 用户信息
+}
+
+// 歌曲接口
 export interface Song {
   id: number
   name: string
