36d5a26fd28bed6238b3dcfcac4bc0c21e3b3a58
ranan <ranan@email.com>
2024-09-08 20:35:12 +0800

    fix: 解决部分歌词时间戳格式不同而无法正确解析的问题

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,12 +1,27 @@
+/**
+ * @description 解析歌词
+ * @param lyric lyric形式的歌词
+ * @returns
+ * lyric 形式的歌词中时间戳的格式可能为 [00:00.000] 或 [00:00:00]
+ */
 export function parseLyric(lyric: string) {
   const lines = lyric.split('\n')
   const result = []
-  const reg = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
-  const globalReg = new RegExp(reg, 'g')
-  const singleReg = new RegExp(reg)
+  const reg = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/ // [00:00.000]
+  const reg2 = /\[(\d{2}):(\d{2}):(\d{2})\]/ // [00:00:00]
+  let globalReg
+  let singleReg
   for (const line of lines) {
-    const time = line.match(globalReg)
-    if (!time) continue
+    globalReg = new RegExp(reg, 'g')
+    singleReg = new RegExp(reg)
+    let time = line.match(globalReg)
+    if (!time) {
+      // 如果没有匹配到则切换匹配模式
+      globalReg = new RegExp(reg2, 'g')
+      singleReg = new RegExp(reg2)
+      time = line.match(globalReg)
+      if (!time) continue
+    }
     const content = line.replace(globalReg, '').trim()
     for (const t of time) {
       const timeArr = t.match(singleReg)
